de5b5922429bd3d9ed810142947a660f921fbbbb
Kirill Tepljakov <super.css2017@yandex.ru>
2021-12-11 16:16:54 +0300

    затёр ненужное

--- js/load-home.js
+++ js/load-home.js
@@ -21,12 +21,3 @@ const createLoader = (onSuccess, onError) => {
 
 export { createLoader };
 
-// const createLoader = () => {
-// 	fetch('http://localhost:3000/data')
-// 	.then((response) => response.json())
-// 	.then((data) => {
-// 				console.log(data);
-// 			})
-// }
-
-// export {createLoader};
