84d4e273ab625e1bd459277b2511f7bfc0b84149
Kanasimy <admin@active-way.ru>
2021-12-24 16:58:42 +0700

    Добавила функционал кнопке еще

--- js/createCard.js
+++ js/createCard.js
@@ -1,13 +1,34 @@
+/**
+ *
+ * @returns {Promise<void>}
+ */
+const btnMore = document.querySelector(".btn-more")
+/* Текущая страница */
+let currentPage = 1;
+/* количество объявлений на странице */
+const countItem = 3;
+/* сюда запишем массив полученный с сервера */
+let arrayCard = [];
 
-async function createLoader() {
+btnMore.addEventListener('click', createLoader);
+
+async function loader() {
 	let response = await fetch("http://localhost:3000/data");
 	let arrayCard = await response.json();
+	return arrayCard;
+};
 
+function createLoader(array = []) {
+	if(array.length) {
+		arrayCard = array;
+	}
 	let card = document.querySelector('.cards-holder');
-
-	let key;
-	for(key in arrayCard) {
-		card.innerHTML += `
+	let key = countItem * (currentPage - 1);
+	console.log(key);
+	for(key; key < arrayCard.length; key++) {
+		/* индекс не больше длинны массива, и не больше количества выводимых страниц */
+		if(arrayCard.length > countItem * currentPage && key <= countItem * currentPage - 1) {
+			card.innerHTML += `
 		<a class="card-link" href="#">
 			<div class="card">
 				<div class="card__badges">
@@ -26,7 +47,20 @@ async function createLoader() {
 			</div>
 		</a>
 		`
+		} else if(arrayCard.length < countItem * currentPage ) {
+			alert("конец")
+			document.querySelector(".btn-more").disabled = true;
+			return;
+		} else if(key > countItem * currentPage - 1) {
+			currentPage++;
+			return currentPage;
+		}
 	}
 }
+function showError () {}
 
-createLoader();
+loader()
+	.then(
+		result => createLoader(result),
+		error => showError(error)
+		);
